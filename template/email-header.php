b27c20750b726a0456e05f5cd7adce8acddd3dde
David Bishop <david.bishop@agilisys.co.uk>
2021-11-15 21:24:13 +0000

    Updated email

---
+++ template/email-header.php
@@ -0,0 +1,19 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>The Bridge</title>
+    <style>
+        body {
+            font-family:monospace;
+        }
+        table {
+            width:100%;
+        }
+        th {
+            text-align: left;
+        }
+        td {
+            background: #eee;
+            padding: 5px;
+        }
+        </style>
